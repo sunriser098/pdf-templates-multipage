50216a09ba65300204f9486439d3c761b78605f6
sunriser <sunriser098@gmail.com>
2024-02-11 11:14:24 -0600

    Add footers to everypage by topic.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Creating pDF templates with multipages using
  - fpdf package
  - pandas to read data from csv file
  - mulipages creation
+ - footer on each page
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
@@ -11,6 +13,17 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.line(10, 22, 200, 22)
 
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=12)
+    pdf.set_text_color(100, 100, 254)
+    pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=12)
+        pdf.set_text_color(100, 100, 254)
+        pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
 pdf.output("output.pdf")
