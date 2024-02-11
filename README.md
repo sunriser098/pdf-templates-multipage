2f503a00872b4db6996ccef6a7844ca43080cf0f
sunriser <sunriser098@gmail.com>
2024-02-11 10:50:26 -0600

    PDF singlepage creation for multiple topics each on one page commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# What is this project about?
+Creating pDF templates with multipages using 
+ - fpdf package
+ - pandas to read data from csv file
+ - mulipages creation
---
+++ main.py
@@ -0,0 +1,13 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(0, 0, 254)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10, 22, 200, 22)
+pdf.output("output.pdf")
