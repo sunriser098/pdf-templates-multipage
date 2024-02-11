77762c7ff71756d0e07f62c0b08a6b30ac681f37
sunriser <sunriser098@gmail.com>
2024-02-11 11:29:51 -0600

    Add lined pdf pages.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Creating pDF templates with multipages using
  - pandas to read data from csv file
  - mulipages creation
  - footer on each page
+ - pdf pages with lines
+
--- main.py
+++ main.py
@@ -11,19 +11,23 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0, 0, 254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(10, 22, 200, 22)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+    # pdf.line(10, 22, 200, 22)
 
     # Set the footer
     pdf.ln(265)
-    pdf.set_font(family="Times", style="I", size=12)
+    pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(100, 100, 254)
-    pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         # Set the footer
         pdf.ln(277)
-        pdf.set_font(family="Times", style="I", size=12)
+        pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(100, 100, 254)
-        pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
-pdf.output("output.pdf")
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+pdf.output("output_lined.pdf")
